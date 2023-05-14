f6b5712b80101491697245ba6b1a1cdb3df6882e
hyoaru <jjcabreraaaa@gmail.com>
2023-05-14 20:03:49 +0800

    [add] meal cards hover effect

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,19 +1,34 @@
-document.querySelectorAll('.meal-card').forEach((mealCard) => {
-    const mealCardImageContainer = mealCard.firstElementChild;
+const mealCard = document.querySelectorAll('.meal-card')
+const categoryButton = document.querySelector('#categoryButton');
+
+
+mealCard.forEach((card) => {
+    const mealCardImageContainer = card.firstElementChild;
     const mealCardImage = mealCardImageContainer.lastElementChild;
-    const mealCardTitle = mealCard.lastElementChild.firstElementChild;
+    const mealCardTitle = card.lastElementChild.firstElementChild;
+
     mealCardImage.addEventListener('load', () => {
         mealCardImageContainer.children[0].remove();
         mealCardImageContainer.children[0].remove();
         mealCardImage.classList.remove('d-none');
         mealCardTitle.classList.remove('placeholder');
     })
+
+    card.style.transition = "all 0.5s"
+
+    card.addEventListener('mouseover', () => {
+        card.classList.toggle('border-primary')
+        card.style.transform = 'scale(1.02)'
+    })
+
+    card.addEventListener('mouseout', () => {
+        card.classList.toggle('border-primary')
+        card.style.transform = 'scale(1)'
+    })
 })
 
-const categoryButton = document.querySelector('#categoryButton');
 
 categoryButton.addEventListener('click', () => {
-
     console.log(categoryButton.attributes)
     if (categoryButton.classList.contains('rounded-bottom-0')) {
         categoryButton.style.transition = "all 3s"
